ab064baeab9dbe0030a6fce084edbc61d392cb4b
Yurii Zhurakovskyi <yurii_zhurakovskyi@epam.com>
2020-03-10 18:49:54 +0200

    Added third solution. Binary search with recursion

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # find_peak
 
-There are two approaches to solve the task:
+There are three approaches to solve the task:
 1. Trivial linear comparison of the data. In my code realized in the "find_peak_trivial()" func. Complexity O(N).
 2. Optimisation can by provided as an immediate break the search loop, when peak is found. This approach realized in the "find_peak_buffer()" function. In the average case complexity is O(N/2). Best case - almost O(1). The worst case O(N)
+3. Recursive binary search with average complexity O(log(N)). For the simplicity I have used index-based access to the elements of array instead of prefferable in the such cases .at() function. Further optimisation may expect to omit recursion and using, i.e. one global vector and iterators to left and right borders.
--- main.cpp
+++ main.cpp
@@ -94,6 +94,53 @@ void find_peak_buffer(const std::vector<uint32_t>& v)
     }
 }
 
+void find_peak_bsearch(const std::vector<uint32_t>& v)
+{
+    if (v.size() < 3)
+    {
+        cout << "Array is too small" << endl;
+        return;
+    }
+
+    if (v.size() == 3)
+    {
+        if(v[0] < v[1] && v[1] > v[2])
+        {
+            cout << "Peak found: " << v[1] << endl;    
+        }
+        else
+        {
+            cout << "Peak not found" << endl;
+        }
+        
+        return;
+    }
+
+    int left_border = 0;
+    int right_border = v.size() - 1;
+    
+    int test_index = right_border - (right_border - left_border) / 2;
+    if(v[test_index] > v[test_index - 1] && v[test_index] > v[test_index + 1])
+    {
+        cout << "Peak found: " << v[test_index] << endl;
+    }
+    else
+    {
+        if(v[test_index] <= v[test_index + 1])
+        {
+            std::vector<uint32_t>sub_vector(v.begin() + test_index, v.end());
+            find_peak_bsearch(sub_vector);
+        }
+        else if(v[test_index - 1] > v[test_index])
+        {
+            std::vector<uint32_t>sub_vector(v.begin(), v.begin() + test_index + 1);
+            find_peak_bsearch(sub_vector);
+        }
+        cout << "Peak not found" << endl;
+    }
+}
+
+
 int main()
 {
      vector<uint32_t> v{1, 1, 2, 3, 5, 8, 13, 21, 20, 19, 18, 17, 16, 15};
@@ -105,7 +152,11 @@ int main()
     // vector<uint32_t> v{0, 1, 1};
     // vector<uint32_t> v{1, 1, 0};
     print(v);
-    //find_peak_trivial(v);
-    find_peak_buffer(v);
+    
+    //find_peak_trivial(v); // Solution 1.
+
+    //find_peak_buffer(v);  // Solution 2.
+
+    find_peak_bsearch(v);   // Solution 3.
     return 0;
 }
